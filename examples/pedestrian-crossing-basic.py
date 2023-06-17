9d37763c2500c100b49ca65230f741d0f955ac30
Sarah Townson <26084137+Isioviel@users.noreply.github.com>
2023-06-17 09:17:08 +0100

    update examples

--- examples/pedestrian-crossing-basic.py
+++ examples/pedestrian-crossing-basic.py
@@ -4,13 +4,24 @@ import radio
 radio.config(channel=7)
 radio.on()
 
+'''
+Change the number below to match your car's number
+'''
+car_channel = 21
+
 # Code in a 'while True:' loop repeats forever
 while True:
     if button_a.was_pressed():
-        for x in range(5000):
+        for x in range(2000):
             radio.send("stop")
+            radio.config(channel=car_channel)
+            radio.send("stop")
+            radio.config(channel=7)
             sleep(1)
-        sleep(1000)
+        #sleep(1000)
     else:
         radio.send("go")
+        radio.config(channel=car_channel)
+        radio.send("go")
+        radio.config(channel=7)
         sleep(1)
